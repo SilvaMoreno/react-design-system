19334c156b663d167d57a5d817a02e06d965dacd
Elton Moreno <elton989@hotmail.com>
2022-10-12 22:52:53 -0100

    app color and font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import "./styles/global.css";
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-orange-600">Hello world</h1>
+      <button className="bg-cyan-500 font-medium rounded px-3 py2 hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  );
 }
