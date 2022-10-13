e7ffe0db5e00df7c96542c44361b8cc127335377
Elton Moreno <elton989@hotmail.com>
2022-10-12 23:56:36 -0100

    component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { Button } from "./components/Button";
 import "./styles/global.css";
 
 export function App() {
   return (
     <>
       <h1 className="text-2xl font-bold text-orange-600">Hello world</h1>
-      <button className="bg-cyan-500 font-medium rounded px-3 py2 hover:bg-cyan-300">
-        Enviar
-      </button>
+      <Button>Enviar</Button>
     </>
   );
 }
